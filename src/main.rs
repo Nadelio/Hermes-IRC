2d2be1230c264fc5954725c331181d0a095967f0
Nadelio <carefulsniffle@gmail.com>
2025-09-26 11:48:17 -0500

    Got a basic TUI setup

--- src/main.rs
+++ src/main.rs
@@ -64,16 +64,22 @@ fn main() {
 		// write out message history
 		let buf = &output_buffer.to_byte_vec();
 		io::stdout().write_all(buf).unwrap();
-		io::stdout().flush().unwrap();
 		// move back to user prompt section
 		print!("{ANSI}H{ANSI}2C");
 		// get user prompt
+		io::stdout().flush().unwrap();
 		let mut input = String::new();
 		io::stdin().read_line(&mut input).unwrap();
 		let result = parse_input(&input);
 		output_buffer.push(result.0);
 		//TODO: quit cmd (doesn't print quitting message, need to do that!)
 		if result.1 {
+			clear_term_screen();
+			print!("> {ANSI}B\r");
+			let buf = &output_buffer.to_byte_vec();
+			io::stdout().write_all(buf).unwrap();
+			print!("{ANSI}H{ANSI}2C");
+			io::stdout().flush().unwrap();
 			break;
 		}
 	}
