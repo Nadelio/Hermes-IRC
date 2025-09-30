c90cd3d9322dd81264ed4e520180aa4ad0f78617
Nadelio <carefulsniffle@gmail.com>
2025-09-29 21:02:26 -0500

    Reorganizing src folder and main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,6 @@
 use std::io::{self, Write};
 
-const ANSI: &str = "\u{1b}["; // ansi escape code
-const RESET: &str = "\u{1b}[0m";
-const NORMAL: &str = "\u{1b}[36m"; // cyan
-const SYSTEM: &str = "\u{1b}[33m"; // yellow
-const PING: &str = "\u{1b}[35m"; // magenta/pink
-const CHANNEL: &str = "\u{1b}[94;47m"; // bright blue
-const ERROR: &str = "\u{1b}[31m"; // red
+pub mod tui;
 
 // TODO: move trait and impl code to utils.rs
 trait ToU8Vec {
@@ -21,39 +15,6 @@ impl ToU8Vec for Vec<String> {
 		}
 		vu8
 	}
-}
-
-// TODO: Move to src/tui/tui.rs
-fn parse_input(input: &str) -> (String, bool) {
-	if input.to_lowercase().trim() == "quit" || input.to_lowercase().trim() == "q" {
-		return (format!("$ {SYSTEM}[Client] Closing Client...{RESET}"), true);
-	}
-
-	let parts: Vec<String> = input
-		.split_whitespace()
-		.map(|part| {
-			let mut part_str = part.to_string();
-
-			match part_str.chars().next().unwrap() {
-				'@' => part_str.insert_str(0, PING),
-				'#' => part_str.insert_str(0, CHANNEL),
-				'!' => part_str.insert_str(0, ERROR),
-				_ => part_str.insert_str(0, NORMAL),
-			}
-
-			part_str.push_str(RESET);
-			part_str
-		})
-		.collect();
-
-	let highlighted = parts.join(" ");
-
-	(format!("$ {highlighted}\n"), false)
-}
-
-// TODO: Move to src/tui/tui.rs
-fn clear_term_screen() {
-	print!("{esc}[2J{esc}[H", esc = 27 as char);
 }
 
 fn main() {
---
+++ src/tui/tui.rs
@@ -0,0 +1,40 @@
+mod tui::tui;
+
+const ANSI: &str = "\u{1b}["; // ansi escape code
+const RESET: &str = "\u{1b}[0m";
+const NORMAL: &str = "\u{1b}[36m"; // cyan
+const SYSTEM: &str = "\u{1b}[33m"; // yellow
+const PING: &str = "\u{1b}[35m"; // magenta/pink
+const CHANNEL: &str = "\u{1b}[94;47m"; // bright blue
+const ERROR: &str = "\u{1b}[31m"; // red
+
+pub fn parse_input(input: &str) -> (String, bool) {
+	if input.to_lowercase().trim() == "quit" || input.to_lowercase().trim() == "q" {
+		return (format!("$ {SYSTEM}[Client] Closing Client...{RESET}"), true);
+	}
+
+	let parts: Vec<String> = input
+		.split_whitespace()
+		.map(|part| {
+			let mut part_str = part.to_string();
+
+			match part_str.chars().next().unwrap() {
+				'@' => part_str.insert_str(0, PING),
+				'#' => part_str.insert_str(0, CHANNEL),
+				'!' => part_str.insert_str(0, ERROR),
+				_ => part_str.insert_str(0, NORMAL),
+			}
+
+			part_str.push_str(RESET);
+			part_str
+		})
+		.collect();
+
+	let highlighted = parts.join(" ");
+
+	(format!("$ {highlighted}\n"), false)
+}
+
+pub fn clear_term_screen() {
+	print!("{esc}[2J{esc}[H", esc = 27 as char);
+}
