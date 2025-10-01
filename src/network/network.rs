52e18ad5872ae5f6a0dce4643dfe417f4009b3d4
Nadelio <carefulsniffle@gmail.com>
2025-10-01 17:13:36 -0500

    Did something to network.rs?

--- src/network/network.rs
+++ src/network/network.rs
@@ -3,5 +3,5 @@ use crate::tui::tui::{RESET, SYSTEM};
 const PORT: u32 = 6667;
 
 pub fn network_info() -> String {
-	format!("$ {SYSTEM}[SERVER] Port: {PORT}, IP: Local Host{RESET}\n")
+	format!("$ {SYSTEM}[Server] Port: {PORT}, IP: Local Host{RESET}\n")
 }
